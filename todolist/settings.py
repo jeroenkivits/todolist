f370128e7a381f84b2ebcac8705caac529121c0e
jeroenkivits <jeroenkivits98@gmail.com>
2017-12-22 05:49:58 +0100

    about page updated

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x+8h!@7h0rb*%f2^@t*8@a*@o9vkg*w(_@*8u(gzn%9n1gsl1('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
